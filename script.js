19e2c3e3ab8b069ea3710c4ece376691b0b803d3
tanishq <tanishq.malik31@gmail.com>
2024-02-20 22:53:42 +0530

    : add javascript

---
+++ script.js
@@ -0,0 +1,31 @@
+function markCompleted(taskId){
+    var checkbox = document.getElementById(taskId);
+    var listItem = checkbox.parentElement;
+
+    if(checkbox.checked){
+        listItem.style.textDecoration="line-through";
+    }
+    else{
+        listItem.style.textDecoration="none";
+    }
+}
+
+
+// document.getElementById('addtask').addEventListener('click', addTask);
+// const inputBox = document.getElementById("search");
+// const listContainer=document.querySelector(".tasklist");
+
+// function addTask(){
+//     if(inputBox.value === ''){
+//         alert("you must write something !");
+//     }
+//     else{
+//         let li= document.createElement("li");
+//         li.innerHTML= '<input type="checkbox">' + inputBox.value;
+//         listContainer.appendChild(li);
+//     }
+//     inputBox.value="";
+// }
+
+
+
