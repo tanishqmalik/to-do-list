d69227cfd7b1032651672ac7c436816641d75ae5
tanishq <tanishq.malik31@gmail.com>
2024-02-21 22:08:36 +0530

    updates

--- script.js
+++ script.js
@@ -11,24 +11,20 @@ function markCompleted(taskId){
 }
 
 
-// document.getElementById('addtask').addEventListener('click', addTask);
-// const inputBox = document.getElementById("search");
-// const listContainer=document.querySelector(".tasklist");
 
-// function addTask(){
-//     if(inputBox.value === ''){
-//         alert("you must write something !");
-//     }
-//     else{
-//         let li= document.createElement("li");
-//         li.innerHTML= '<input type="checkbox">' + inputBox.value;
-//         listContainer.appendChild(li);
-//     }
+document.getElementById('addtask').addEventListener('click', addTask);
+const inputBox = document.getElementById("search");
+const listContainer = document.getElementById("tasklist"); // Corrected
+
+function addTask(event){
+    event.preventDefault();
+    if(inputBox.value === ''){
+        alert("you must write something !");
+    }
+    else{
+        let li= document.createElement("li");
+        li.innerHTML= '<input type="checkbox">' + inputBox.value;
+        listContainer.appendChild(li);
+    }
     // inputBox.value="";
-// }
-
-
-// hello
-
-
-
+}
