02ecfe1d11033d6740a6cd5e7e1e74c227a2fba1
tanishq <tanishq.malik31@gmail.com>
2024-02-22 19:42:20 +0530

    updates

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// const { doc } = require("prettier");
+
 function markCompleted(taskId){
     var checkbox = document.getElementById(taskId);
     var listItem = checkbox.parentElement;
@@ -14,7 +16,7 @@ function markCompleted(taskId){
 
 document.getElementById('addtask').addEventListener('click', addTask);
 const inputBox = document.getElementById("search");
-const listContainer = document.getElementById("tasklist"); // Corrected
+const listContainer = document.getElementById("tasklist"); 
 
 function addTask(event){
     event.preventDefault();
@@ -22,9 +24,25 @@ function addTask(event){
         alert("you must write something !");
     }
     else{
+        listContainer.innerHTML='';   //existing task ko clear krdo inner html mein jakr .  ye listContainer
+        //hmne ek id se liya hua hai jo ki "tasklist" hai or vo ul ki id hai.
         let li= document.createElement("li");
         li.innerHTML= '<input type="checkbox">' + inputBox.value;
+        // listContainer.replaceChild(li, listContainer.firstElementChild)
         listContainer.appendChild(li);
+
     }
-    // inputBox.value="";
+    inputBox.value="";
 }
+
+
+document.getElementById('deletetask').addEventListener('click', deletetask);
+function deletetask(){
+    // event.preventDefault();
+    let todelete=document.getElementById('label1')
+    todelete.innerHTML=''
+    let todelet1= Document.getElementById('task1');
+    todelet1.parentNode.removeChild(todelet1)
+}
+
+addtask.addEventListener('click',addTask)
